baea2deb85af214a5922dd0d7aa9dd681b9baa03
hayden16-html <orsmondhayden@gmail.com>
2026-07-29 14:52:42 +1200

    I have removed the code for the food page.
    I have added code for a functioning top nav
    
    I have added css to style the nav bar for the html
    
    I have added a script for the funtionality of the menu button

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuButton =
+    document.querySelector(".menu-button");
+
+const menu =
+    document.querySelector(".menu");
+
+
+menuButton.addEventListener("click", function () {
+
+    menu.classList.toggle("menu-open");
+
+});
